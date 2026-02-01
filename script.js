357fb62d5ec6cf410a3ffa7e2b909a62b44789c5
Mekaeda <mekaedab@gmail.com>
2026-02-01 17:46:16 +0000

    Excercise 2 Interim

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-/* CODE GOES HERE */
+let list = [];
+// identical to "var"
+
+function preload(){
+  list = loadJSON ("data.json");
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(240,67,151);
+  // fill(240,200,67);
+  // strokeWeight(0,0,0);
+  // circle(mouseX,mouseY,100);
+  // rect(70,20,50,50);
+  // fill ("0");
+
+  //Title
+  textAlign("center");
+  textSize(20);
+  text("Canada's Exports", 200,40);
+
+  //Iterate through the list and place elements
+  Object.values(list).map((item, index) => {
+    rect(50, index * 30 + 70, item.amount, 10);
+    textSize(10);
+    textAlign ("left");
+    text(item.name, 50, index * 30 + 65);
+  });
+}
